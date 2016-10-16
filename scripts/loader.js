13e2a30e974738581421e71105b408fdd663c4ef
Maicon Gomes <mgomes@localhost.localdomain>
2016-10-16 17:00:11 -0200

    first commit

---
+++ scripts/loader.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mgomes on 15/10/16.
+ */
+$(document).ready(function() {
+
+  // Fakes the loading setting a timeout
+  setTimeout(function() {
+    $('body').addClass('loaded');
+  }, 3500);
+
+});
